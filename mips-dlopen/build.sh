fefa7e30bfc42db3ca3e8ef5fbb415e39a44121f
xiaoyur347 <xiaoyur347@gmail.com>
2015-02-25 13:00:26 +0800

    [test] add android toolchain 4.9 support for android lib

--- mips-dlopen/build.sh
+++ mips-dlopen/build.sh
@@ -4,5 +4,7 @@ g++ main.cpp -o x86_test -ldl
 mipsel-linux-g++ -shared -fPIC libtest.cpp -o libtest_mips.so
 mipsel-linux-g++ main.cpp -o mips_test -ldl
 
+ndk-build -C ./android/lib clean
 ndk-build -C ./android/lib
+ndk-build -C ./android/test clean
 ndk-build -C ./android/test
